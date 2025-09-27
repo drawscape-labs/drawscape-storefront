c2716eb85ccbf22e0abe64d8cf3281a0c5780724
russell <russelltaylor05@gmail.com>
2025-09-27 12:13:32 -0700

    Wrap Meta Pixel in ClientOnly component
    Ensure Meta Pixel script only loads on client side to prevent SSR issues

--- app/root.tsx
+++ app/root.tsx
@@ -194,22 +194,24 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <Meta />
         <Links />
         {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '560982449624198');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <ClientOnly>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '560982449624198');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        </ClientOnly>
         <noscript>
           <img 
             height="1" 
